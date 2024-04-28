9369dc1e9ef4a808b4732c9df68422421de61502
Piyushshind <piyushshinde.199@gmail.com>
2024-04-28 22:33:59 +0530

    InfoHoverButton changed

--- src/components/InfoHoverButton/InfoHoverButton.jsx
+++ src/components/InfoHoverButton/InfoHoverButton.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './InfoHoverButton.css';
+import { FaInfoCircle } from "react-icons/fa";
+
 
 const InfoHoverButton = () => {
     return (
         <>
             <div className="card">
-                <span>&#8505;</span>
+                <span>  <FaInfoCircle /></span>
                 <h1>THE PROVIDERS</h1>
                 <p>We are providing free source codes for everyone of complete websites</p>
                 <p>Thanks for Subscribe our Youtube Channel &nbsp; <button>SUBSCRIBE</button>&nbsp;
-                <img src="https://static.thenounproject.com/png/3680435-200.png" alt="subscribe"/></p>
+                    <img src="https://static.thenounproject.com/png/3680435-200.png" alt="subscribe" />
+                </p>
             </div>
         </>
     );
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { images } from '../../constants';
 import './AboutUs.css';
+import InfoHoverButton from '../../components/InfoHoverButton/InfoHoverButton';
 
 const AboutUs = () => (
   <>
@@ -26,7 +27,9 @@ const AboutUs = () => (
           <img src={images.spoon} alt="about_spoon" className="spoon__img" />
           <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
           <button type="button" className="custom__button">Know More</button>
-
+          <span>
+            <InfoHoverButton />
+          </span>
         </div>
       </div>
 
